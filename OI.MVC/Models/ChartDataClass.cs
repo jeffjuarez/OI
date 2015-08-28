458436097618d3813aac6e50ccc2502f55df9505
jeffjuarez <jeff.juarez@gmail.com>
2015-08-28 18:43:33 +0800

    Initial Upload
    First Commit of the Project

---
+++ OI.MVC/Models/ChartDataClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace OI.MVC.Models
+{
+  
+        public class ChartDataClass
+        {
+            public DateTime ExecutionDate { get; set; }
+            public double ExecutionValue { get; set; }
+        }
+  
+}
---
+++ OI.MVC/Models/DocumentViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+namespace OI.MVC.Models
+{
+    public class DocumentViewModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Filename")]
+        public string Filename { get; set; }
+
+        [Display(Name = "SortCenter")]
+        public string SortCenter { get; set; }
+
+        [Display(Name = "ReceivedDateFrom")]
+        public DateTime ReceivedDateFrom { get; set; }
+
+        [Display(Name = "PreparedBy")]
+        public string PreparedBy { get; set; }
+
+        [Display(Name = "UserUploaded")]
+        public string UserUploaded { get; set; }
+
+
+        [Display(Name = "DateUploaded")]
+        public DateTime DateUploaded { get; set; }
+
+        [Display(Name = "EmployeeId")]
+        public int EmployeeId { get; set; }
+
+
+    }
+
+
+    //public class ChartDataClass
+    //{
+    //    public DateTime ExecutionDate { get; set; }
+    //    public double ExecutionValue { get; set; }
+    //}
+
+
+}
